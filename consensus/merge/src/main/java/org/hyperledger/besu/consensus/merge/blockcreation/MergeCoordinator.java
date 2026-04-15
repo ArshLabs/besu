021b10577c5deb4eb1cb16abaf7483b7c86441a9
Fabio Di Fabio <fabio.difabio@consensys.net>
2026-04-15 12:20:00 +0200

    Order IL txs before starting their selection and include IL in payloadId creation
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- consensus/merge/src/main/java/org/hyperledger/besu/consensus/merge/blockcreation/MergeCoordinator.java
+++ consensus/merge/src/main/java/org/hyperledger/besu/consensus/merge/blockcreation/MergeCoordinator.java
@@ -40,8 +40,6 @@ import org.hyperledger.besu.ethereum.core.MiningConfiguration;
 import org.hyperledger.besu.ethereum.core.MutableWorldState;
 import org.hyperledger.besu.ethereum.core.Transaction;
 import org.hyperledger.besu.ethereum.core.Withdrawal;
-import org.hyperledger.besu.ethereum.core.encoding.EncodingContext;
-import org.hyperledger.besu.ethereum.core.encoding.TransactionDecoder;
 import org.hyperledger.besu.ethereum.eth.manager.EthScheduler;
 import org.hyperledger.besu.ethereum.eth.sync.backwardsync.BackwardSyncContext;
 import org.hyperledger.besu.ethereum.eth.sync.backwardsync.BadChainListener;
@@ -60,7 +58,6 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
@@ -69,10 +66,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import com.google.common.annotations.VisibleForTesting;
-import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -248,7 +243,7 @@ public class MergeCoordinator implements MergeMiningCoordinator, BadChainListene
       final Optional<List<Withdrawal>> withdrawals,
       final Optional<Bytes32> parentBeaconBlockRoot,
       final Optional<Long> slotNumber,
-      final Optional<List<Bytes>> inclusionListTransactions) {
+      final List<Transaction> inclusionListTransactions) {
 
     // we assume that preparePayload is always called sequentially, since the RPC Engine calls
     // are sequential, if this assumption changes then more synchronization should be added to
@@ -262,7 +257,8 @@ public class MergeCoordinator implements MergeMiningCoordinator, BadChainListene
             feeRecipient,
             withdrawals,
             parentBeaconBlockRoot,
-            slotNumber);
+            slotNumber,
+            inclusionListTransactions);
 
     if (blockCreationTasks.containsKey(payloadIdentifier)) {
       LOG.debug(
@@ -312,9 +308,6 @@ public class MergeCoordinator implements MergeMiningCoordinator, BadChainListene
       }
     }
 
-    final List<Transaction> decodedIlTransactions =
-        getInclusionListTransactions(inclusionListTransactions);
-
     // Create the async block building task and store it
     tryToBuildBetterBlock(
         timestamp,
@@ -325,35 +318,9 @@ public class MergeCoordinator implements MergeMiningCoordinator, BadChainListene
         parentBeaconBlockRoot,
         slotNumber,
         parentHeader,
-        decodedIlTransactions);
+        inclusionListTransactions);
 
     return payloadIdentifier;
-  }
-
-  private static List<Transaction> getInclusionListTransactions(
-      final Optional<List<Bytes>> inclusionListTransactions) {
-    // Decode inclusion list transaction bytes to Transaction objects
-    final List<Transaction> decodedIlTransactions =
-        inclusionListTransactions
-            .map(
-                ilBytes ->
-                    ilBytes.stream()
-                        .map(
-                            bytes -> {
-                              try {
-                                return TransactionDecoder.decodeOpaqueBytes(
-                                    bytes, EncodingContext.BLOCK_BODY);
-                              } catch (final Exception e) {
-                                LOG.warn(
-                                    "Failed to decode inclusion list transaction, skipping: {}",
-                                    e.getMessage());
-                                return null;
-                              }
-                            })
-                        .filter(Objects::nonNull)
-                        .collect(Collectors.toList()))
-            .orElse(List.of());
-    return decodedIlTransactions;
   }
 
   private void cancelAnyExistingBlockCreationTasks(final PayloadIdentifier payloadIdentifier) {
--- consensus/merge/src/main/java/org/hyperledger/besu/consensus/merge/blockcreation/TransitionCoordinator.java
+++ consensus/merge/src/main/java/org/hyperledger/besu/consensus/merge/blockcreation/TransitionCoordinator.java
@@ -34,7 +34,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 
 /** The Transition coordinator. */
@@ -154,7 +153,7 @@ public class TransitionCoordinator extends TransitionUtils<MiningCoordinator>
       final Optional<List<Withdrawal>> withdrawals,
       final Optional<Bytes32> parentBeaconBlockRoot,
       final Optional<Long> slotNumber,
-      final Optional<List<Bytes>> inclusionListTransactions) {
+      final List<Transaction> inclusionListTransactions) {
     return mergeCoordinator.preparePayload(
         parentHeader,
         timestamp,
